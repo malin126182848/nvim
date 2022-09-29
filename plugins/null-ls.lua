113da4d57d1a4c2d8acede216817b9e0fcaeb809
malin126182848 <malin126182848@126.com>
2022-09-29 23:29:08 +0800

    fix: pretter

--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -10,7 +10,7 @@ local sources = {
 
    -- webdev stuff
    b.formatting.deno_fmt,
-   b.formatting.prettier { filetypes = {"html", "markdown", "css"} },
+   b.formatting.prettier,
 
    -- Lua
    b.formatting.stylua,
