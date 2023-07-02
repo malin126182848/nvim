659c5dcba711a0981aaa668cfdea311aabdd4706
malin126182848 <malin126182848@126.com>
2023-07-02 19:08:32 +0800

    feat: add vscode-neovim key binding

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,4 +5,8 @@
 if vim.g.vscode then
   vim.keymap.set("n", "<leader>ca", "<cmd>call VSCodeNotify('editor.action.quickFix')<cr>")
   vim.keymap.set("n", "<leader>lh", "<cmd>call VSCodeNotify('clangd.switchheadersource')<cr>")
+  vim.keymap.set("n", "<C-j>", "<cmd>call VSCodeNotify('workbench.action.focusBelowGroup')<cr>")
+  vim.keymap.set("n", "<C-k>", "<cmd>call VSCodeNotify('workbench.action.focusAboveGroup')<cr>")
+  vim.keymap.set("n", "<C-h>", "<cmd>call VSCodeNotify('workbench.action.focusLeftGroup')<cr>")
+  vim.keymap.set("n", "<C-l>", "<cmd>call VSCodeNotify('workbench.action.focusRightGroup')<cr>")
 end
